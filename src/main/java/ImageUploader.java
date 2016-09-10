75ed12c813a4f23acca9b874398f2097142ae68e
Kyle Rohlfing <krohlfing94@gmail.com>
2016-09-10 14:15:13 -0500

    Can now successfully upload images.

---
+++ src/main/java/ImageUploader.java
@@ -0,0 +1,24 @@
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by krohlfing on 9/10/2016.
+ */
+public class ImageUploader {
+    private Cloudinary cloudinary;
+    public ImageUploader() {
+        Map config = new HashMap();
+        config.put("cloud_name", "di1dnijpw");
+        config.put("api_key", "126987751573914");
+        config.put("api_secret", "PU3pzVV2R3pDjfk5Nxge1tk919I");
+        cloudinary = new Cloudinary(config);
+    }
+
+    public void upload(String imagePath) throws IOException {
+        System.out.println(cloudinary.uploader().upload("C:/Users/krohlfing/Desktop/images/KyleRohlfing.jpg", ObjectUtils.emptyMap()));
+    }
+}
