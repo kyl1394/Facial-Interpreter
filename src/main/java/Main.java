5429e27d37f2cbf0984f3a0e951a4b21a9248e03
Kyle Rohlfing <krohlfing94@gmail.com>
2016-09-10 13:03:07 -0500

    unneeded

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,6 @@
 
 import GUI.DesktopUI;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 public class Main {
     public static void main(String args[]) {
         DesktopUI.main(args);
